fd90c4aab6a89938e4e0bb0dbe19658401471260
Alysson <alyssuuuuu@outlook.com>
2025-06-18 01:02:43 -0300

    fix: api not ignored

---
+++ src/api/models/LimitParam.ts
@@ -0,0 +1,8 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * The number of results per page.
+ */
+export type LimitParam = number;
---
+++ src/api/models/PageParam.ts
@@ -0,0 +1,8 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * The page number to retrieve.
+ */
+export type PageParam = number;
